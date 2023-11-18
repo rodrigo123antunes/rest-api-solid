8c48fa51ed2f7c8ac5342970ae2269392e381eed
“Rodrigo <rodrigobpzantunes2016@gmail.com>
2023-11-18 15:01:03 -0300

    feat: vitest env

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,14 @@
+import { Environment } from 'vitest'
+
+export default <Environment>(<unknown>{
+  name: 'prisma',
+  async setup() {
+    console.log('Setup')
+
+    return {
+      async teardown() {
+        console.log('Teardown')
+      },
+    }
+  },
+})
