da93bcd3e3fb0d54486f066ddc3135360466f394
“Rodrigo <rodrigobpzantunes2016@gmail.com>
2023-11-18 15:47:52 -0300

    ci: file

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -1,11 +1,10 @@
 import { FastifyInstance } from 'fastify'
-
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
-
 import { create } from './create'
 import { validate } from './validate'
 import { history } from './history'
 import { metrics } from './metrics'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
@@ -14,5 +13,9 @@ export async function checkInsRoutes(app: FastifyInstance) {
   app.get('/check-ins/metrics', metrics)
 
   app.post('/gyms/:gymId/check-ins', create)
-  app.patch('/check-ins/:checkInId/validate', validate)
+  app.patch(
+    '/check-ins/:checkInId/validate',
+    { onRequest: [verifyUserRole('ADMIN')] },
+    validate,
+  )
 }
